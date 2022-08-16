06fe37a95b129e6f7cd52a0f2079654f67797aad
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-08-16 13:40:18 +0530

    Update before_install.sh

--- app/scripts/before_install.sh
+++ app/scripts/before_install.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt -y install curl
-sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt install -y nodejs
 sudo npm install -y
