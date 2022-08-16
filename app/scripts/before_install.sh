ca57a64c78b8818b84450d5621146ac08a8a6c08
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-08-16 12:18:24 +0530

    Update before_install.sh

--- app/scripts/before_install.sh
+++ app/scripts/before_install.sh
@@ -1,4 +1,4 @@
-sudo yum update
-sudo yum -y install curl
+sudo apt update
+sudo apt -y install curl
 sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-sudo yum install -y nodejs
+sudo apt install -y nodejs
